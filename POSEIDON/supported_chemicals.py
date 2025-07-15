39b8deb046630d4182b21620f394eb26ad253017
omiCet <101759214+omiCet@users.noreply.github.com>
2025-07-15 16:46:18 -0400

    Updated VULCAN supported species list, renamed OCS

--- POSEIDON/supported_chemicals.py
+++ POSEIDON/supported_chemicals.py
@@ -47,12 +47,12 @@ aerosol_supported_species = np.array(['ADP', 'Al2O3', 'Al2O3_KH', 'C', 'CH4_liqu
                                       'TiC', 'TiO2_anatase', 'TiO2_rutile', 'VO', 'ZnS',
                                       'MgSiO3_r_m_std_dev_01','MgSiO3_r_m_std_dev_1','MgSiO3_g_w_calc_mean','MgSiO3_g_w_calc_trap'])
 
-# VULCAN Supported Species as of September 2024
+# VULCAN Supported Species as of July 2025
 vulcan_supported_species = np.array(['OH', 'H2', 'H2O', 'H', 'O', 'CH', 'C', 'CH2', 'CH3', 'CH4', 'C2', 'C2H2', 'C2H', 
                                      'C2H3', 'C2H4', 'C2H5', 'C2H6', 'CO', 'CO2', 'CH2OH', 'H2CO', 'HCO', 'CH3O', 'CH3OH', 
                                      'CH3CO', 'O2', 'H2CCO', 'HCCO', 'N', 'NH', 'CN', 'HCN', 'NO', 'NH2', 'N2', 'NH3', 
                                      'N2H2', 'N2H', 'N2H3', 'N2H4', 'HNO', 'H2CN', 'HNCO', 'NO2', 'N2O', 'C4H2', 'CH2NH2', 
                                      'CH2NH', 'CH3NH2', 'CH3CHO', 'HNO2', 'NCO', 'HO2', 'H2O2', 'HC3N', 'CH3CN', 'CH2CN', 
                                      'C2H3CN', 'SH', 'HSO', 'H2S', 'C3H3', 'C3H2', 'C3H4', 'C6H5', 'C6H6', 'S', 'S2', 
-                                     'SO', 'CS', 'COS', 'CS2', 'NS', 'HS2', 'SO2', 'S4', 'S8', 'HCS', 'S3', 'C4H3', 'C4H5', 
-                                     'S2O', 'CH3SH', 'CH3S', 'O_1', 'CH2_1', 'N_2D', 'He', 'S8_l_s'])
+                                     'SO', 'CS', 'OCS', 'CS2', 'NS', 'HS2', 'SO2', 'S4', 'S8', 'HCS', 'S3', 'C4H3', 'C4H5', 
+                                     'S2O', 'CH3SH', 'CH3S', 'O_1', 'CH2_1', 'N_2D', 'He', 'H2CS', 'S8_l_s'])
